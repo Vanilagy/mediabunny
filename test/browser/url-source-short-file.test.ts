5ce171bd27c123e9810ef377e9a828fc50f9ea27
Jonny Burger <jonathanburger11@gmail.com>
2025-09-02 15:19:52 +0200

    Make an actual assertion in the test

--- test/browser/url-source-short-file.test.ts
+++ test/browser/url-source-short-file.test.ts
@@ -1,14 +1,19 @@
-import { test } from 'vitest';
+import { expect, test } from 'vitest';
 import { UrlSource } from '../../src/source.js';
 import { ALL_FORMATS } from '../../src/input-format.js';
 import { Input } from '../../src/input.js';
 
 test('Should be able to get track from a very small video file (512kb)', async () => {
-    const source = new UrlSource('/frames.webm');
-    const input = new Input({
-        source,
-        formats: ALL_FORMATS,
-    });
-    const primaryVideoTrack = await input.getPrimaryVideoTrack();
-    console.log(input)
+	const source = new UrlSource('/frames.webm');
+	const input = new Input({
+		source,
+		formats: ALL_FORMATS,
+	});
+	const primaryVideoTrack = await input.getPrimaryVideoTrack();
+	if (!primaryVideoTrack) {
+		throw new Error('No video track found');
+	};
+
+	const duration = await primaryVideoTrack.computeDuration();
+	expect(duration).toBeCloseTo(3.33333);
 });
