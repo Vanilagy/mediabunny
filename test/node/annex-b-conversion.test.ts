c2cfbbc786339d8d90475519dd23be02439678f1
Vanilagy <1696106+Vanilagy@users.noreply.github.com>
2025-12-29 21:26:02 +0100

    Fix type errors

--- test/node/annex-b-conversion.test.ts
+++ test/node/annex-b-conversion.test.ts
@@ -7,8 +7,8 @@ import { Output } from '../../src/output.js';
 import { Mp4OutputFormat } from '../../src/output-format.js';
 import { BufferTarget } from '../../src/target.js';
 import { Conversion } from '../../src/conversion.js';
-import { EncodedPacketSink } from '../../src/media-sink.js';
 import { extractAvcNalUnits } from '../../src/codec-data.js';
+import { PacketReader } from '../../src/cursors.js';
 
 const __dirname = new URL('.', import.meta.url).pathname;
 
@@ -22,8 +22,8 @@ test('Annex B to length-prefixed conversion, MP4', async () => {
 	expect(originalDecoderConfig.description).toBeUndefined();
 	expect(originalVideoTrack.codec).toBe('avc');
 
-	const originalSink = new EncodedPacketSink(originalVideoTrack);
-	const originalFirstPacket = await originalSink.getFirstPacket();
+	const originalReader = new PacketReader(originalVideoTrack);
+	const originalFirstPacket = await originalReader.readFirst();
 	expect([...originalFirstPacket!.data.slice(0, 4)]).toEqual([0, 0, 0, 1]);
 
 	const originalNalUnits = extractAvcNalUnits(originalFirstPacket!.data, originalDecoderConfig);
@@ -45,8 +45,8 @@ test('Annex B to length-prefixed conversion, MP4', async () => {
 	expect(newDecoderConfig.description).toBeDefined();
 	expect(newVideoTrack.codec).toBe('avc');
 
-	const newSink = new EncodedPacketSink(newVideoTrack);
-	const newFirstPacket = await newSink.getFirstPacket();
+	const newReader = new PacketReader(newVideoTrack);
+	const newFirstPacket = await newReader.readFirst();
 	expect([...newFirstPacket!.data.slice(0, 4)]).not.toEqual([0, 0, 0, 1]); // Successfully converted
 
 	const newNalUnits = extractAvcNalUnits(newFirstPacket!.data, newDecoderConfig);
