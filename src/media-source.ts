c423d62fd939d5184c2212d8d3dfc70871791a0d
Vanilagy <1696106+Vanilagy@users.noreply.github.com>
2026-06-02 18:18:53 +0200

    Change keyFrame parameter priority

--- src/media-source.ts
+++ src/media-source.ts
@@ -462,14 +462,15 @@ class VideoEncoderWrapper {
 
 				const mergedEncodeOptions = { ...sampleToEncode.encodeOptions, ...encodeOptions };
 
-				// Ensure a key frame every keyFrameInterval seconds. It is important that all video tracks
-				// follow the same "key frame" rhythm, because aligned key frames are required to start new
-				// fragments in ISOBMFF or clusters in Matroska (or at least desirable).
 				const finalEncodeOptions = {
 					...mergedEncodeOptions,
-					keyFrame: mergedEncodeOptions.keyFrame
-						|| keyFrameInterval === 0
-						|| multipleOfKeyFrameInterval !== this.lastMultipleOfKeyFrameInterval,
+					keyFrame: mergedEncodeOptions.keyFrame !== undefined
+						? mergedEncodeOptions.keyFrame
+						// Ensure a key frame every keyFrameInterval seconds. It is important that all video tracks
+						// follow the same "key frame" rhythm, because aligned key frames are required to start new
+						// fragments in ISOBMFF or clusters in Matroska (or at least desirable).
+						: keyFrameInterval === 0
+							|| multipleOfKeyFrameInterval !== this.lastMultipleOfKeyFrameInterval,
 				};
 				this.lastMultipleOfKeyFrameInterval = multipleOfKeyFrameInterval;
 
